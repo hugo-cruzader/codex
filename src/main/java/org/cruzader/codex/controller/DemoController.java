578175a685a2ffacf3e5d29c55928a8e5383af5f
Hugo Huipet <hugo.huipet@gmail.com>
2026-03-28 20:32:35 -0700

    Revert "feat: Added versioning but swagger broke"
    This reverts commit 7033b0fbb10a2806a0ef47cf9b57da7fd5fd6200.

--- src/main/java/org/cruzader/codex/controller/DemoController.java
+++ src/main/java/org/cruzader/codex/controller/DemoController.java
@@ -1,30 +1,17 @@
 package org.cruzader.codex.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api")
-@Tag(name = "Demo API", description = "Versioned Demo Endpoints")
 public class DemoController {
 
-    @GetMapping(value = "/{version}/hello", version = "1.0")
-    @Operation(summary = "Get message V1")
+    @GetMapping("/hello")
     public ResponseEntity<String> getHelloEndpoint() {
         log.info("Entered demo endpoint Hello");
         return ResponseEntity.ok("HELLO");
-    }
-
-    @GetMapping(value="/{version}/hello", version = "2.0")
-    @Operation(summary = "Get message V2")
-    public ResponseEntity<String> getHelloEndpointV2() {
-        log.info("Entered demo endpoint V2 Hello");
-        return ResponseEntity.ok("V2 is here");
     }
 }
